93fa107d8ddabe1eb2bddfe0f5c9028f0872878e
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-28 19:27:36 +0530

    fix data mix

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Balance from './Balance'
 import IncomeExpenses from './IncomeExpenses'
+import { useLocation } from "react-router-dom";
 
 const Home = (props) => {
+
+
+  const location = useLocation();
+
+  useEffect(() => {
+    fetchData();
+  }, [location.pathname]);
+
+
   return (
     <div className='home'>
       <div className='sub_home'>
-        <Balance balance={props.balance}/>
+        <Balance balance={props.balance} />
       </div>
       <div className='sub_home'>
-        <IncomeExpenses income={props.income} expense={props.expense}/>
+        <IncomeExpenses income={props.income} expense={props.expense} />
       </div>
     </div>
   )
