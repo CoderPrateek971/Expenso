80b229cfd57911842eda3a4289f58df2e1e4aa27
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-28 17:33:22 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,59 +2,60 @@ import { useState, useEffect } from 'react'
 import AddTransaction from './components/AddTransaction'
 import './App.css'
 import TransactionList from './components/TransactionList';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import axios from "axios";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 
 function App() {
   const [transactions, setTransactions] = useState([]);
-  const [amount,setAmount]= useState();
-  const [text,setText]= useState("");
+  const [amount, setAmount] = useState();
+  const [text, setText] = useState("");
 
-  
+
   const amounts = transactions.map(t => t.amount);
   const totalBalance = amounts.reduce((acc, curr) => acc + curr, 0);
-  const Income = amounts.filter((amount)=> amount>0);
-  const TotalIncome = Income.reduce((acc,curr)=> acc+(curr),0);
-  const expense = amounts.filter((amount)=> amount<0);
-  const TotalExpense = expense.reduce((acc,curr)=> acc+(-curr),0);
+  const Income = amounts.filter((amount) => amount > 0);
+  const TotalIncome = Income.reduce((acc, curr) => acc + (curr), 0);
+  const expense = amounts.filter((amount) => amount < 0);
+  const TotalExpense = expense.reduce((acc, curr) => acc + (-curr), 0);
 
- 
+
   const addTransaction = (transaction) => {
 
-    axios.post("https://expenso-osyg.onrender.com/api/v1/transactions", transaction, {  
+    axios.post("https://expenso-osyg.onrender.com/api/v1/transactions", transaction, {
       headers: {
-      Authorization: localStorage.getItem("token")   
-    }
-  })
-    .then((response)=>{
-      setTransactions((prev)=>[...prev,response.data.data])
+        Authorization: localStorage.getItem("token")
+      }
     })
-    .catch((error)=>{
-      console.log("error:",error)
-    })
-  
+      .then((response) => {
+        setTransactions((prev) => [...prev, response.data.data])
+      })
+      .catch((error) => {
+        console.log("error:", error)
+      })
+
   }
 
 
-  const deleteTransaction=(id)=>{
+  const deleteTransaction = (id) => {
     axios.delete(`https://expenso-osyg.onrender.com/api/v1/transactions/${id}`, {
       headers: {
-        Authorization: localStorage.getItem("token")   
+        Authorization: localStorage.getItem("token")
       }
     })
-    .then(()=>{
-      setTransactions(prev => prev.filter(t => t._id !== id))   
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
+      .then(() => {
+        setTransactions(prev => prev.filter(t => t._id !== id))
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   const myRouter = createBrowserRouter([
@@ -66,50 +67,50 @@ function App() {
         </div>
       )
     },
-  
+
     {
       path: "/home",
       element: localStorage.getItem("token") ? (
-        <div>
+        <ProtectedRoute>
           <Navbar />
-          <Home 
-            balance={totalBalance} 
-            income={TotalIncome} 
-            expense={TotalExpense} 
+          <Home
+            balance={totalBalance}
+            income={TotalIncome}
+            expense={TotalExpense}
           />
-        </div>
+        </ProtectedRoute>
       ) : (
-        <Dashboard />   
+        <Dashboard />
       )
     },
-  
+
     {
       path: "/TransactionList",
       element: localStorage.getItem("token") ? (
-        <div>
+        <ProtectedRoute>
           <Navbar />
-          <TransactionList 
-            list={transactions} 
-            delete={deleteTransaction} 
+          <TransactionList
+            list={transactions}
+            delete={deleteTransaction}
           />
-        </div>
+        </ProtectedRoute>
       ) : (
         <Dashboard />
       )
     },
-  
+
     {
       path: "/AddTransaction",
       element: localStorage.getItem("token") ? (
-        <div>
+        <ProtectedRoute>
           <Navbar />
           <AddTransaction new_transaction={addTransaction} />
-        </div>
+        </ProtectedRoute>
       ) : (
         <Dashboard />
       )
     },
-  
+
     {
       path: "/Login",
       element: (
@@ -118,7 +119,7 @@ function App() {
         </div>
       )
     },
-  
+
     {
       path: "/SignUp",
       element: (
@@ -131,34 +132,34 @@ function App() {
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-  
+
     if (!token) {
       console.log("No token found ❌");
       return;
     }
-  
+
     axios.get("https://expenso-osyg.onrender.com/api/v1/transactions", {
       headers: {
         Authorization: token
       }
     })
-    .then((response)=>{
-      setTransactions(response.data.data)
-    })
-    .catch((error)=>{
-      console.log("error : ", error);
-    })
-  
+      .then((response) => {
+        setTransactions(response.data.data)
+      })
+      .catch((error) => {
+        console.log("error : ", error);
+      })
+
   }, [])
 
 
-  
+
 
   return (
     <div className='app'>
-    
 
-      <RouterProvider router={myRouter}/>
+
+      <RouterProvider router={myRouter} />
 
     </div>
   )
