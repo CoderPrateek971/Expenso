b3b0ca64c738831f402374554f5c79b0fbe4e287
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-28 19:22:01 +0530

    fix data mix

--- Frontend/src/components/AddTransaction.jsx
+++ Frontend/src/components/AddTransaction.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import '../App.css'
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 
 const AddTransaction = (props) => {
