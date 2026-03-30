7390b1e2aa33858bf8f9847037ac54a4e4c1d717
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-31 00:54:49 +0530

    home page updated

--- Frontend/src/components/IncomeExpenses.jsx
+++ Frontend/src/components/IncomeExpenses.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const IncomeExpenses = (props) => {
   return (
     <div className='income' >
-        <h3 className='heading'>Income</h3>
+        <h3 className='heading'>Overview</h3>
         <div>Income:  {props.income}</div>
         <br />
         <div>Expense:  {props.expense}</div>
