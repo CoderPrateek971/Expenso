fc7472682e06b61fb168800fba58bb3c6c82585f
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-31 00:39:08 +0530

    backend fix

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -2,18 +2,20 @@ const jwt = require("jsonwebtoken");
 
 exports.auth = (req, res, next) => {
     try {
-        const token = req.header("Authorization");
+        const authHeader = req.header("Authorization");
 
-        if (!token) {
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(401).json({
                 success: false,
                 message: "Token missing"
             });
         }
 
+        const token = authHeader.split(" ")[1];
+
         const decoded = jwt.verify(token, "secretkey");
 
-        req.user = decoded;   
+        req.user = decoded;
 
         next();
     } catch (error) {
